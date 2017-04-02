9ea8934f985313b0161e7eca09f84a5e788588eb
Dmitry Demenchuk <dmitry@demenchuk.me>
2017-04-02 01:26:21 +0100

    Add possibility to add messages

--- src/public/main.js
+++ src/public/main.js
@@ -1,24 +1,15 @@
-var app = angular.module('starter', ['ionic']);
+var app = angular.module('app', ['ionic']);
 
-app.controller('PageCtrl', function($scope, $rootScope) {
-  var messageOptions = [
-    { content: '<p>Wow, this is really something huh?</p>' },
-    { content: '<p>Yea, it\'s pretty sweet</p>' },
-    { content: '<p>I think I like Ionic more than I like ice cream!</p>' },
-    { content: '<p>Gee wiz, this is something special.</p>' },
-    { content: '<p>Is this magic?</p>' },
-    { content: '<p>Am I dreaming?</p>' },
-    { content: '<p>Yea, it\'s pretty sweet</p>' },
-    { content: '<p>I think I like Ionic more than I like ice cream!</p>' },
-  ];
+app.controller('PageCtrl', function($scope) {
+  $scope.messages = []; 
 
-  var messageIter = 0;
+  $scope.add = function(message) {
+    $scope.messages.push({
+      type: 'text',
+      content: message 
+    });
 
-  $scope.messages = messageOptions.slice(0, messageOptions.length);
-
-  $scope.add = function() {
-    var nextMessage = messageOptions[messageIter++ % messageOptions.length];
-    $scope.messages.push(angular.extend({}, nextMessage));
+    $scope.message = '';
   };
 });
 
