ef2a4f0fcb8c1e0289b3a9462d4f59d93348adab
Dmitry Demenchuk <dmitry@demenchuk.me>
2017-04-10 00:48:52 +0100

    Update style.

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 var port = 80;
 
-server.listen(port, function () {
+server.listen(port, function() {
   console.log('Server listening at port %d', port);
 });
 
@@ -13,14 +13,13 @@ server.listen(port, function () {
 app.use(express.static(__dirname + '/public'));
 
 // Chatroom
-
 var numUsers = 0;
 
-io.on('connection', function (socket) {
+io.on('connection', function(socket) {
   var addedUser = false;
 
   // when the client emits 'new message', this listens and executes
-  socket.on('new message', function (data) {
+  socket.on('new message', function(data) {
     // we tell the client to execute 'new message'
     socket.broadcast.emit('new message', {
       username: socket.username,
@@ -29,7 +28,7 @@ io.on('connection', function (socket) {
   });
 
   // when the client emits 'add user', this listens and executes
-  socket.on('add user', function (username) {
+  socket.on('add user', function(username) {
     if (addedUser) return;
 
     // we store the username in the socket session for this client
@@ -47,21 +46,21 @@ io.on('connection', function (socket) {
   });
 
   // when the client emits 'typing', we broadcast it to others
-  socket.on('typing', function () {
+  socket.on('typing', function() {
     socket.broadcast.emit('typing', {
       username: socket.username
     });
   });
 
   // when the client emits 'stop typing', we broadcast it to others
-  socket.on('stop typing', function () {
+  socket.on('stop typing', function() {
     socket.broadcast.emit('stop typing', {
       username: socket.username
     });
   });
 
   // when the user disconnects.. perform this
-  socket.on('disconnect', function () {
+  socket.on('disconnect', function() {
     if (addedUser) {
       --numUsers;
 
