fd5825dac9e7928214849f3d763bdf3765c02e7a
Dmitry Demenchuk <dmitry@demenchuk.me>
2018-11-07 19:26:14 +0000

    Respond to all requests

--- src/backend.js
+++ src/backend.js
@@ -6,6 +6,10 @@ const port = process.env.PORT || 5000;
 
 app.use(Express.static(__dirname + '/build'));
 
+app.get('*', function(req, res) {
+  res.sendfile(__dirname + '/build/index.html');
+});
+
 const server = app.listen(port, () => {
   console.log(`server is running on port ${port}`)
 });
